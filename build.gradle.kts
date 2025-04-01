666f90e978e24e5fb1aa3f117322d82df4bdd8ab
MisterCheezeCake <75341435+MisterCheezeCake@users.noreply.github.com>
2025-03-31 23:04:24 -0400

    backend: Multiversion Workflow

---
+++ build.gradle.kts
@@ -0,0 +1,71 @@
+
+plugins {
+	id("fabric-loom") version "1.10-SNAPSHOT"
+}
+
+
+version = property("mod_version")!! as String + "+mc" + property("minecraft_version")!!
+group = property("maven_group")!!
+
+base {
+	archivesName.set(property("archives_base_name") as String)
+}
+
+repositories {
+	mavenCentral()
+	repositories {
+		exclusiveContent {
+			forRepository {
+				maven {
+					name = "Modrinth"
+					url =  uri("https://api.modrinth.com/maven")
+				}
+			}
+			filter {
+				includeGroup("maven.modrinth")
+			}
+		}
+	}
+}
+
+dependencies {
+	minecraft("com.mojang:minecraft:${property("minecraft_version")}")
+	mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
+	modImplementation ("net.fabricmc:fabric-loader:${property("loader_version")}")
+
+	// Fabric API is not an end user dep -- it's required by Controlling hence why it's marked as runtimeOnly
+	modRuntimeOnly("net.fabricmc.fabric-api:fabric-api:${property("fabric_version")}")
+
+	modImplementation("maven.modrinth:controlling:${property("controlling_version")}")
+	modImplementation("maven.modrinth:searchables:${property("searchables_version")}")
+	include(implementation("com.moulberry:mixinconstraints:${(property("mcon_version"))}")!!)
+
+	
+}
+
+tasks.processResources {
+	inputs.property("version", project.version)
+
+	filesMatching("fabric.mod.json") {
+		expand("version" to project.version)
+	}
+}
+
+tasks.withType<JavaCompile>().configureEach {
+	options.release.set(21)
+}
+
+java {
+	withSourcesJar()
+	sourceCompatibility = JavaVersion.VERSION_21
+	targetCompatibility = JavaVersion.VERSION_21
+}
+
+
+tasks.named<Jar>("jar") {
+
+	from("LICENSE") {
+		rename { "${it}_${project.base.archivesName.get()}" }
+	}
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+	repositories {
+		maven {
+			name = "Fabric"
+			url = uri("https://maven.fabricmc.net/")
+		}
+		mavenCentral()
+		gradlePluginPortal()
+		maven("https://maven.kikugie.dev/snapshots")
+	}
+}
+
+plugins {
+	id("dev.kikugie.stonecutter") version "0.6-alpha.13"
+}
+
+stonecutter {
+	kotlinController = true
+	centralScript = "build.gradle.kts"
+
+	create(rootProject) {
+		versions("1.21.1", "1.21.3", "1.21.4", "1.21.5")
+		vcsVersion = "1.21.4"
+	}
+}
